e8f865b7f60f476474c5047bf1316de6f418de8e
Kelvenyn <kelvenyn@users.noreply.github.com>
2026-06-04 12:18:51 -0300

    feat: add Kit Contents section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero";
 import { PainPoints } from "@/components/sections/PainPoints";
+import { KitContents } from "@/components/sections/KitContents";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <PainPoints />
+      <KitContents />
     </main>
   );
 }
